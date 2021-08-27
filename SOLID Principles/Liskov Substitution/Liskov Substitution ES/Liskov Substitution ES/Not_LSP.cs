77df90ec69f058924e86f79263d693387211607f
LeanTolo <leandrotoloza8@gmail.com>
2021-08-27 16:38:09 -0300

    Liskov Substitution ES

---
+++ SOLID Principles/Liskov Substitution/Liskov Substitution ES/Liskov Substitution ES/Not_LSP.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Liskov_Substitution_ES
+{
+    //El Principio de sustitución de Liskov se basa en que
+    //Cada clase que hereda de otra puede usarse como su padre sin necesidad de conocer las diferencias entre ellas.
+
+
+    //En el siguiente ejemplo observaran Apple como clase base y Green_NOT_SLP como secundaria,
+    //existiendo una relacion padre e hijo, podemos almacenar el objeto secundario en referencia a la clase principal
+    //es decir, Apple apple = new Green_NOT_SLP(); y cuando llamamos a apple.GetColor(), entonces obtenemos el color de Green_NOT_SLP,
+    //no el color de Apple. Eso significa que una vez que se reemplaza el objeto secundario, es decir,
+    //que Apple almacena el objeto Green_NOT_SLP, cambiando tambien el comportamiento. Yendo en contra del principio LSP.
+
+    class Not_LSP
+    {
+        //static void Main(string[] args)
+        //{
+            Apple apple = new Green_NOT_SLP();
+           // Console.WriteLine(apple.GetColor());
+       // }
+    }
+    public class Apple
+    {
+        public virtual string GetColor()
+        {
+            return "Red";
+        }
+    }
+    public class Green_NOT_SLP : Apple
+    {
+        public override string GetColor()
+        {
+            return "Green_NOT_SLP";
+        }
+    }
+}
---
+++ SOLID Principles/Liskov Substitution/Liskov Substitution ES/Liskov Substitution ES/Program_LSP.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Liskov_Substitution_ES
+{
+    //LSP establece que incluso el objeto hijo se reemplaza con el padre sin cambiar el comportamiento.
+    //Si obtenemos el color de Apple en lugar de Green, entonces sigue LSP.
+    //Modifiquemos el ejemplo NOT_LSP para seguir el principio de sustitución de Liskov.
+    //Aquí, primero, necesitamos una clase base genérica como Fruit, que será la clase base
+    //tanto para Applee como para Green. Ahora puede reemplazar el objeto de la clase Fruit
+    //con sus subtipos, ya sea Applee y Green, y se comportará correctamente. Ahora, puede ver
+    //en el siguiente código, creamos la clase super Fruit como una clase abstracta con el método
+    //abstracto GetColor y luego las clases Applee y Green heredadas de la clase Fruit e implementamos el método GetColor.
+    //Respetando ahora el LSP
+
+    class Program_LSP
+    {
+        static void Main(string[] args)
+        {
+            Fruit fruit = new Green();
+            Console.WriteLine(fruit.GetColor());
+            fruit = new Applee();
+            Console.WriteLine(fruit.GetColor());
+        }
+    }
+    public abstract class Fruit
+    {
+        public abstract string GetColor();
+    }
+    public class Applee : Fruit
+    {
+        public override string GetColor()
+        {
+            return "Red";
+        }
+    }
+    public class Green : Fruit
+    {
+        public override string GetColor()
+        {
+            return "Green";
+        }
+    }
+}
