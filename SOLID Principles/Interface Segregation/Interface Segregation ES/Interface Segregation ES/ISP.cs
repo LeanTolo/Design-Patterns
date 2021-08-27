2bfc21fce1c946a02a4c923f0add4e3afc63191b
LeanTolo <leandrotoloza8@gmail.com>
2021-08-27 17:31:54 -0300

    Interface Segregation ES

---
+++ SOLID Principles/Interface Segregation/Interface Segregation ES/Interface Segregation ES/ISP.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Interface_Segregation_ES
+{
+    class ISP
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello ISP!");
+        }
+    }
+
+    //En NOT_ISP, vemos que hemos creado una interfaz base que
+    //tiene las propiedades comunes de todos los empleados. Luego, creamos
+    //dos interfaces más que implementan la interfaz base y estas interfaces
+    //son para el empleado a tiempo completo y contratado, respectivamente.
+    //Solo contienen propiedades y métodos para el tipo particular de empleado.
+    //Finalmente, en nuestras clases para el empleado a tiempo completo y contratado,
+    //solo implementamos la interfaz requerida (tiempo completo o contrato).
+    //No se requiere ningún método o propiedad adicional.
+    public interface IBaseWorker
+    {
+        string ID { get; set; }
+        string Name { get; set; }
+        string Email { get; set; }
+
+    }
+
+    public interface IFullTimeWorkerSalary : IBaseWorker
+    {
+        float Salary { get; set; }
+        float CalculateNetSalary();
+    }
+
+    public interface IContractWorkerSalary : IBaseWorker
+    {
+        float HoursInMonth { get; set; }
+        float CalaculateWorkedSalary();
+    }
+
+    public class FullTimeEmployeeFixed : IFullTimeWorkerSalary
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public float Salary { get; set; }
+        public float CalculateNetSalary() => Salary ;
+    }
+
+    public class ContractEmployeeFixed : IContractWorkerSalary
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public float HoursInMonth { get; set; }
+        public float CalaculateWorkedSalary() => HoursInMonth;
+    }
+}
---
+++ SOLID Principles/Interface Segregation/Interface Segregation ES/Interface Segregation ES/NOT_ISP.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface_Segregation_ES
+{
+    //El Principio de Segregación de Interfaces (ISP) establece que una clase no debe tener
+    //que implementar ningún elemento de interfaz que no sea requerido por la clase en particular.
+    //Esto asegurará que la clase y, en última instancia, toda la aplicación sea muy robusta y fácil de mantener y expandir si es necesario.
+
+    //En el ejemplo a continuacion, no se respeta el ISP
+    //ya que la clase FullTimeEmployee no necesita implementar la función CalculateWorkedSalary
+    //y la clase ContractEmployee no necesita implementar la función CalculateNetSalary.
+
+    class NOT_ISP
+    {
+    }
+    public interface IWorker
+    {
+        string ID { get; set; }
+        string Name { get; set; }
+        string Email { get; set; }
+        float Salary { get; set; }
+        float HoursInMonth { get; set; }
+        float CalculateNetSalary();
+        float CalaculateWorkedSalary();
+    }
+
+    public class FullTimeEmployee : IWorker
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public float Salary { get; set; }
+        public float HoursInMonth { get; set; }
+        public float CalculateNetSalary() => Salary;
+        public float CalaculateWorkedSalary() => throw new NotImplementedException();
+    }
+
+    public class ContractEmployee : IWorker
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public float Salary { get; set; }
+        public float HoursInMonth { get; set; }
+        public float CalculateNetSalary() => throw new NotImplementedException();
+        public float CalaculateWorkedSalary() => HoursInMonth;
+    }
+}
